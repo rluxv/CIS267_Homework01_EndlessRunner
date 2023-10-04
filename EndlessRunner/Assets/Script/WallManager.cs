ae77602d2e79b5099c01e7e3a5d0673862812424
Nicholas F <51171750+rluxv@users.noreply.github.com>
2023-10-04 15:03:19 -0400

    Update WallManager.cs

--- EndlessRunner/Assets/Script/WallManager.cs
+++ EndlessRunner/Assets/Script/WallManager.cs
@@ -7,22 +7,20 @@ public class WallManager : MonoBehaviour
 {
     private Rigidbody2D wallRigidBody;
     public float wallMovementSpeed;
-    private int time;
+    float timer = 0.0f;
+    int seconds;
     // Start is called before the first frame update
     void Start()
     {
         wallRigidBody = GetComponent<Rigidbody2D>();
-        time = 0;
     }
 
     private void FixedUpdate()
     {
-        //
-        time++;
         //Debug.Log("Time: " + time);
         // wait a few seconds before moving the wall
         // this will also be used to increase the wall speed as time goes on
-        if (time > 100)
+        if (seconds >= 3)
         {
             wallRigidBody.AddForce(transform.right * wallMovementSpeed);
         }
@@ -32,7 +30,8 @@ public class WallManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        seconds = (int)(timer % 60);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
