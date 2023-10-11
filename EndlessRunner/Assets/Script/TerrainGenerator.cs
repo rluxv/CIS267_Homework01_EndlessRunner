37bc9588be538620e544fd01a1010e59ce5730a6
Nicholas F <51171750+rluxv@users.noreply.github.com>
2023-10-11 11:25:44 -0400

    infinite terrain

--- EndlessRunner/Assets/Script/TerrainGenerator.cs
+++ EndlessRunner/Assets/Script/TerrainGenerator.cs
@@ -4,28 +4,26 @@ using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
 {
+    private const float PLAYER_DISTANCE_SPAWN_TERRAIN = 50f;
     [SerializeField] private Transform terrainStart;
     [SerializeField] private Transform Terrain1;
+    [SerializeField] private GameObject player;
     private Vector3 lastEndPos;
     void Start()
     {
-        //Instantiate(Terrain1, new Vector3(-0, -0), Quaternion.identity);
         lastEndPos = new Vector3(terrainStart.Find("EndPos").position.x + 10, terrainStart.Find("EndPos").position.y);
 
         spawnTerrain();
         spawnTerrain();
-        //lastTerrainTFM = spawnTerrain(new Vector3(terrainStart.Find("EndPos").position.x + 10, terrainStart.Find("EndPos").position.y));
-        //lastTerrainTFM = spawnTerrain(new Vector3(lastTerrainTFM.Find("EndPos").position.x + 10, lastTerrainTFM.Find("EndPos").position.y));
-        //lastTerrainTFM = spawnTerrain(new Vector3(lastTerrainTFM.Find("EndPos").position.x + 10, lastTerrainTFM.Find("EndPos").position.y));
-        //lastTerrainTFM = spawnTerrain(new Vector3(lastTerrainTFM.Find("EndPos").position.x + 10, lastTerrainTFM.Find("EndPos").position.y));
-        //lastTerrainTFM = spawnTerrain(new Vector3(lastTerrainTFM.Find("EndPos").position.x + 10, lastTerrainTFM.Find("EndPos").position.y));
-        //spawnTerrain(new Vector3(terrainStart.Find("EndPos").position.x + 10, terrainStart.Find("EndPos").position.y));
-        //spawnTerrain(new Vector3(terrainStart.Find("EndPos").position.x + 10, terrainStart.Find("EndPos").position.y));
-        //spawnTerrain(new Vector3(terrainStart.Find("EndPos").position.x + 10, terrainStart.Find("EndPos").position.y));
-        //spawnTerrain(lastTerrainTFM.Find("EndPos").position);
-        //spawnTerrain(new Vector3(0, 0));
-        //spawnTerrain(new Vector3(0, 0) + new Vector3(40, 0));
-        //spawnTerrain(new Vector3(0, 0) + new Vector3(40 + 40, 0));
+    }
+
+    private void Update()
+    {
+        if(Vector3.Distance(player.transform.position, lastEndPos) < PLAYER_DISTANCE_SPAWN_TERRAIN)
+        {
+            spawnTerrain();
+        }
+
     }
 
     private void spawnTerrain()
