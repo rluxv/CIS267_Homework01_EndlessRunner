7e24731054ede88252ecb72e81f0553d6eb80b7b
Nicholas F <51171750+rluxv@users.noreply.github.com>
2023-10-11 11:20:40 -0400

    Terrainwork

--- EndlessRunner/Assets/Script/TerrainGenerator.cs
+++ EndlessRunner/Assets/Script/TerrainGenerator.cs
@@ -4,17 +4,39 @@ using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
 {
+    [SerializeField] private Transform terrainStart;
     [SerializeField] private Transform Terrain1;
+    private Vector3 lastEndPos;
     void Start()
     {
         //Instantiate(Terrain1, new Vector3(-0, -0), Quaternion.identity);
-        spawnTerrain(new Vector3(0, 0));
-        spawnTerrain(new Vector3(0, 0) + new Vector3(40, 0));
-        spawnTerrain(new Vector3(0, 0) + new Vector3(40 + 40, 0));
+        lastEndPos = new Vector3(terrainStart.Find("EndPos").position.x + 10, terrainStart.Find("EndPos").position.y);
+
+        spawnTerrain();
+        spawnTerrain();
+        //lastTerrainTFM = spawnTerrain(new Vector3(terrainStart.Find("EndPos").position.x + 10, terrainStart.Find("EndPos").position.y));
+        //lastTerrainTFM = spawnTerrain(new Vector3(lastTerrainTFM.Find("EndPos").position.x + 10, lastTerrainTFM.Find("EndPos").position.y));
+        //lastTerrainTFM = spawnTerrain(new Vector3(lastTerrainTFM.Find("EndPos").position.x + 10, lastTerrainTFM.Find("EndPos").position.y));
+        //lastTerrainTFM = spawnTerrain(new Vector3(lastTerrainTFM.Find("EndPos").position.x + 10, lastTerrainTFM.Find("EndPos").position.y));
+        //lastTerrainTFM = spawnTerrain(new Vector3(lastTerrainTFM.Find("EndPos").position.x + 10, lastTerrainTFM.Find("EndPos").position.y));
+        //spawnTerrain(new Vector3(terrainStart.Find("EndPos").position.x + 10, terrainStart.Find("EndPos").position.y));
+        //spawnTerrain(new Vector3(terrainStart.Find("EndPos").position.x + 10, terrainStart.Find("EndPos").position.y));
+        //spawnTerrain(new Vector3(terrainStart.Find("EndPos").position.x + 10, terrainStart.Find("EndPos").position.y));
+        //spawnTerrain(lastTerrainTFM.Find("EndPos").position);
+        //spawnTerrain(new Vector3(0, 0));
+        //spawnTerrain(new Vector3(0, 0) + new Vector3(40, 0));
+        //spawnTerrain(new Vector3(0, 0) + new Vector3(40 + 40, 0));
     }
 
-    private void spawnTerrain(Vector3 position)
+    private void spawnTerrain()
     {
-        Instantiate(Terrain1, position, Quaternion.identity);
+        Transform lastTerrainTFM = spawnTerrain(lastEndPos);
+        lastEndPos = new Vector3(lastTerrainTFM.Find("EndPos").position.x + 10, lastTerrainTFM.Find("EndPos").position.y);
+    }
+
+    private Transform spawnTerrain(Vector3 position)
+    {
+        Transform terrainTFM = Instantiate(Terrain1, position, Quaternion.identity);
+        return terrainTFM;
     }
 }
