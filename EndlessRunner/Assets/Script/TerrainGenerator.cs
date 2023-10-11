db930379a1fac45d4e4e1d49a70a82d30c5112dc
Nicholas F <51171750+rluxv@users.noreply.github.com>
2023-10-11 10:42:15 -0400

    Terrain gen work

--- EndlessRunner/Assets/Script/TerrainGenerator.cs
+++ EndlessRunner/Assets/Script/TerrainGenerator.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class TerrainGenerator : MonoBehaviour
 {
     [SerializeField] private Transform Terrain1;
-    private void Awake()
+    void Start()
     {
-        Instantiate(Terrain1, new Vector3(-0, -0), Quaternion.identity);
+        //Instantiate(Terrain1, new Vector3(-0, -0), Quaternion.identity);
+        spawnTerrain(new Vector3(0, 0));
+        spawnTerrain(new Vector3(0, 0) + new Vector3(40, 0));
+        spawnTerrain(new Vector3(0, 0) + new Vector3(40 + 40, 0));
+    }
+
+    private void spawnTerrain(Vector3 position)
+    {
+        Instantiate(Terrain1, position, Quaternion.identity);
     }
 }
