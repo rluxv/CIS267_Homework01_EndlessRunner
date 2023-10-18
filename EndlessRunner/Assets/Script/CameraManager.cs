262e59e4a84e26e861171d034d3928353c186789
Nicholas F <51171750+rluxv@users.noreply.github.com>
2023-10-17 22:22:52 -0400

    Update CameraManager.cs

--- EndlessRunner/Assets/Script/CameraManager.cs
+++ EndlessRunner/Assets/Script/CameraManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     public float FollowSpeed = 2f;
-    public float yOffset =1f;
+    public float yOffset = 1f;
     public Transform target;
 
     // Update is called once per frame
