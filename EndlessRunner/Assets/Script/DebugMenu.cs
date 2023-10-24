84c65a3bcf5a443bc23f2389e848d812b2189ad0
Nicholas F <51171750+rluxv@users.noreply.github.com>
2023-10-24 14:36:18 -0400

    Debug menu update

--- EndlessRunner/Assets/Script/DebugMenu.cs
+++ EndlessRunner/Assets/Script/DebugMenu.cs
@@ -11,12 +11,19 @@ public class DebugMenu : MonoBehaviour
     public static bool debugMenuActive;
     public TMP_Text fpsCounterTMP;
     public GameObject fpsCounterGO;
+    public TMP_Text systemInfoTMP;
+    public GameObject systemInfoGO;
+
     void Start()
     {
 
-        if(debugMenuActive == false) fpsCounterGO.SetActive(false);
-
+        if (debugMenuActive == false)
+        {
+            fpsCounterGO.SetActive(false);
+            systemInfoGO.SetActive(false);
+        }
         InvokeRepeating("updateFPS", 0, 1);
+        systemInfoTMP.SetText("CPU: " + SystemInfo.processorType + " GPU: " + SystemInfo.graphicsDeviceName + ", " + SystemInfo.graphicsDeviceType);
     }
 
     // Update is called once per frame
@@ -30,11 +37,13 @@ public class DebugMenu : MonoBehaviour
             {
                 debugMenuActive = true;
                 fpsCounterGO.SetActive(true);
+                systemInfoGO.SetActive(true);
             }
             else
             {
                 debugMenuActive = false;
                 fpsCounterGO.SetActive(false);
+                systemInfoGO.SetActive(false);
             }
         }
     }
